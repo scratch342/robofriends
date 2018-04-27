37995fc0606f3f3035d6093f6a26d96677384795
scratch342 <scratchcat342@gmail.com>
2018-04-27 20:34:57 +0930

    v1

---
+++ src/CardList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ( { robots } ) => {
+	const cardComponent = robots.map((user, index) => {
+		return( <Card
+			 key={robots[index].id} 
+			 id={robots[index].id} 
+			 name={robots[index].name} 
+			 email={robots[index].email}
+			 />
+		 )
+	})
+	return (
+		<div>
+			{cardComponent}
+		</div>
+	)
+}
+
+export default CardList;
